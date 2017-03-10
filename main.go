7a8457cf59973020454fe8e376aff134506cf000
Gordon McNaughton <gmcnaughton@gmail.com>
2017-03-10 20:15:52 +1300

    Allow command-line flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "flag"
   "fmt"
   "os"
   "path/filepath"
@@ -8,14 +9,21 @@ import (
   "github.com/gmcnaughton/gofindhdr/findhdr"
 )
 
+// Usage:
+//     go run main.go -in "~/Pictures/Photos Library.photoslibrary/Masters/2017/02" -out ./out -link
+//     go run main.go -in ./test -out ./out
 func main() {
-  // inpath := "/Users/gmcnaughton/Pictures/Photos Library.photoslibrary/Masters/2017/02"
-  inpath := "./test"
-  outpath := "./out"
-  optlink := false
+  var inpath, outpath string
+  var link bool
+
+  // flag.StringVar(&inpath, "in", "/Users/gmcnaughton/Pictures/Photos Library.photoslibrary/Masters/2017/02", "path to input directory to search")
+  flag.StringVar(&inpath, "in", "./test", "path to search")
+  flag.StringVar(&outpath, "out", "./out", "path where matches should be linked")
+  flag.BoolVar(&link, "link", false, "true if matches should be linked")
+  flag.Parse()
 
   // Create output folder
-  if optlink {
+  if link {
     err := os.Mkdir(outpath, 0755)
     if err != nil && !os.IsExist(err) {
       fmt.Println("Error creating out directory", err)
@@ -27,7 +35,7 @@ func main() {
   findhdr.Find(inpath, func(hdr *findhdr.Hdr) {
     count++
 
-    if optlink {
+    if link {
       for _, image := range hdr.Images() {
         link := filepath.Join(outpath, image.Info.Name())
         fmt.Println("Linking", link)
